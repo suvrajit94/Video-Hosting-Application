ed4ceea23cba48fd6eae952f99864cecc295f7c1
Suvrajit Manna <suvrajit.manna@sap.com>
2017-12-10 01:00:52 +0530

    s3 upload/download service implementation

--- src/main/java/com/suvrajit/s3/services/impl/S3ServicesImpl.java
+++ src/main/java/com/suvrajit/s3/services/impl/S3ServicesImpl.java
@@ -7,6 +7,7 @@ package com.suvrajit.s3.services.impl;
 
 import com.suvrajit.s3.services.*;
 import com.suvrajit.config.S3Config;
+import com.suvrajit.config.util.Utility;
 import java.io.File;
 import java.io.IOException;
 
@@ -39,11 +40,48 @@ public class S3ServicesImpl implements S3Services {
 
     @Override
     public void downloadFile(String keyName) {
+        try {
 
+            System.out.println("Downloading an object");
+            S3Object s3object = s3client.getObject(new GetObjectRequest(bucketName, keyName));
+            System.out.println("Content-Type: " + s3object.getObjectMetadata().getContentType());
+            Utility.displayText(s3object.getObjectContent());
+            logger.info("===================== Import File - Done! =====================");
+
+        } catch (AmazonServiceException ase) {
+            logger.info("Caught an AmazonServiceException from GET requests, rejected reasons:");
+            logger.info("Error Message:    " + ase.getMessage());
+            logger.info("HTTP Status Code: " + ase.getStatusCode());
+            logger.info("AWS Error Code:   " + ase.getErrorCode());
+            logger.info("Error Type:       " + ase.getErrorType());
+            logger.info("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            logger.info("Caught an AmazonClientException: ");
+            logger.info("Error Message: " + ace.getMessage());
+        } catch (IOException ioe) {
+            logger.info("IOE Error Message: " + ioe.getMessage());
+        }
     }
 
     @Override
     public void uploadFile(String keyName, String uploadFilePath) {
+        try {
+
+            File file = new File(uploadFilePath);
+            s3client.putObject(new PutObjectRequest(bucketName, keyName, file));
+            logger.info("===================== Upload File - Done! =====================");
+
+        } catch (AmazonServiceException ase) {
+            logger.info("Caught an AmazonServiceException from PUT requests, rejected reasons:");
+            logger.info("Error Message:    " + ase.getMessage());
+            logger.info("HTTP Status Code: " + ase.getStatusCode());
+            logger.info("AWS Error Code:   " + ase.getErrorCode());
+            logger.info("Error Type:       " + ase.getErrorType());
+            logger.info("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            logger.info("Caught an AmazonClientException: ");
+            logger.info("Error Message: " + ace.getMessage());
+        }
 
     }
 
